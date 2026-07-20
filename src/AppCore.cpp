e15167d45ed2218a27e54680d6912615386bae51
Simone Cavalleri <simone.cavalleri.94@gmail.com>
2026-07-20 14:14:55 +0200

    refactor: remove unused AppCore.cpp and Reactor.cpp and update CMakeLists

--- src/AppCore.cpp
+++
@@ -1,5 +0,0 @@
-#include "corium/AppCore.h"
-
-namespace corium {
-// AppCoreT is now a template class AppCoreT<EventVariant>
-} // namespace corium
--- src/Reactor.cpp
+++
@@ -1,5 +0,0 @@
-#include "corium/internal/Reactor.h"
-
-namespace corium {
-// ReactorT is now a template class ReactorT<EventVariant>
-} // namespace corium
